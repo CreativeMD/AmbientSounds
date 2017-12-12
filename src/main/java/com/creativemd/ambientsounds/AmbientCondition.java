8d1a44374efd374d66e4d68010fc14ca4a6cf726
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-12-12 14:41:00 +0100

    Fixed always false not returning always false

--- src/main/java/com/creativemd/ambientsounds/AmbientCondition.java
+++ src/main/java/com/creativemd/ambientsounds/AmbientCondition.java
@@ -284,7 +284,12 @@ public abstract class AmbientCondition {
 							}
 						};
 					}
-					return null;
+					return new AmbientCondition() {
+						@Override
+						public boolean is(AmbientSituation situation, AmbientSoundResult result) {
+							return false;
+						}
+					};
 				}
 				throw new IllegalArgumentException("Expected a boolean!");
 			}
