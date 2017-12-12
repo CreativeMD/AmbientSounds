b1d739d259880637fde35f36131c6fd138500b92
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-12-12 16:10:50 +0100

    Added pitch effect for unterwater sound (suggested by Mr. Bacon) Added underwater-pitch property

--- src/main/java/com/creativemd/ambientsounds/AmbientSoundResult.java
+++ src/main/java/com/creativemd/ambientsounds/AmbientSoundResult.java
@@ -6,6 +6,7 @@ public class AmbientSoundResult {
 	
 	public ArrayList<AmbientCondition> conditions = new ArrayList<>();
 	public float volume = 1;
+	public float pitch = 1;
 	public boolean takeBiome = false;
 	
 	public AmbientSoundResult() {
