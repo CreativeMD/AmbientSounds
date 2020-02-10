0e57f7b559b1de109d044220d37cb428fa917854
CreativeMD <Creative-Multimedia-Design@gmx.de>
2020-02-10 14:01:32 +0100

    Fixed server list incompatible error

--- src/main/java/team/creative/ambientsounds/AmbientSounds.java
+++ src/main/java/team/creative/ambientsounds/AmbientSounds.java
@@ -2,6 +2,7 @@ package team.creative.ambientsounds;
 
 import java.util.function.Predicate;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,10 +10,12 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.resources.IReloadableResourceManager;
 import net.minecraft.resources.IResourceManager;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 import net.minecraftforge.resource.IResourceType;
 import net.minecraftforge.resource.ISelectiveResourceReloadListener;
 
@@ -24,7 +27,6 @@ public class AmbientSounds {
 	public static final String MODID = "ambientsounds";
 	
 	public AmbientSounds() {
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::preInit);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 	}
 	
@@ -34,12 +36,11 @@ public class AmbientSounds {
 	
 	public static AmbientTickHandler tickHandler;
 	
-	private void preInit(final FMLCommonSetupEvent event) {
+	private void doClientStuff(final FMLClientSetupEvent event) {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		
 		tickHandler = new AmbientTickHandler();
 		MinecraftForge.EVENT_BUS.register(tickHandler);
-	}
-	
-	private void doClientStuff(final FMLClientSetupEvent event) {
 		/* ClientCommandHandler.instance.registerCommand(new CommandBase() {
 		 * 
 		 * @Override
