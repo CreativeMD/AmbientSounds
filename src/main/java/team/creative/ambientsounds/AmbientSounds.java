ef91a910bb12602463cfca4a3af9d4d358fb6f0d
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-04-28 13:22:53 +0200

    Fixed executing reloader manager off thread

--- src/main/java/team/creative/ambientsounds/AmbientSounds.java
+++ src/main/java/team/creative/ambientsounds/AmbientSounds.java
@@ -87,7 +87,7 @@ public class AmbientSounds implements ClientLoader {
         if (loader.fabric())
             loader.registerClientStarted(register);
         else
-            register.run();
+            Minecraft.getInstance().execute(register);
         
         CreativeCoreClient.registerClientConfig(MODID);
     }
